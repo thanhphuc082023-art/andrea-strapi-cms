4a55b10bb912eda73bbb5b88bc854c8b317fd25b
Minh Huy <citythree.11798@gmail.com>
2025-08-10 23:42:09 +0700

    feat: add facebookUrl attribute to book schema and TypeScript definitions

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -489,6 +489,7 @@ export interface ApiBookBook extends Struct.CollectionTypeSchema {
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
     downloadUrl: Schema.Attribute.String;
+    facebookUrl: Schema.Attribute.String;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<'oneToMany', 'api::book.book'> &
       Schema.Attribute.Private;
