3e8b0f3c47743e89793385f8436e0f05479f842a
Minh Huy <citythree.11798@gmail.com>
2025-08-28 01:43:43 +0700

    feat: add videoLink attribute to project schema and TypeScript definitions

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -854,6 +854,7 @@ export interface ApiProjectProject extends Struct.CollectionTypeSchema {
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    videoLink: Schema.Attribute.String;
   };
 }
 
